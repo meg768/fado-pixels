9fdef3b4f632350458714d8cc58d5ddf5b40d9c7
meg768 <meg768@users.noreply.github.com>
2017-10-25 15:29:56 +0200

    -

---
+++ neopixel-globe.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+require('dotenv').config();
+
+var sprintf    = require('yow/sprintf');
+var prefixLogs = require('yow/logs').prefix;
+
+var App = function() {
+
+
+	function run() {
+		try {
+			var args = require('yargs');
+
+			args.usage('Usage: $0 <command> [options]')
+
+			args.help();
+
+			args.option('size',      {alias:'S', describe:'Size of the Neopixel strip', default:process.env.NEOPIXEL_STRIP_LENGTH});
+			args.option('debug',     {alias:'D', describe:'Debug mode', default:false});
+
+
+			args.wrap(null);
+			args.demand(1);
+
+			args.argv;
+		}
+
+		catch(error) {
+			console.log(error.stack);
+			process.exit(-1);
+		}
+
+	};
+
+	process.on('unhandledRejection', (reason, p) => {
+		console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+	});
+
+	prefixLogs();
+	run();
+};
+
+module.exports = new App();
