7f7152cf1fdfff57664f415bbac433f46f91ab8c
meg768 <meg768@users.noreply.github.com>
2017-10-26 22:55:14 +0200

    -

--- src/scripts/flash-animation.js
+++ src/scripts/flash-animation.js
@@ -17,6 +17,7 @@ module.exports = class extends Animation {
         this.name = 'Color';
         this.time = undefined;
         this.color = Color('red').rgbNumber();
+        this.options = Object.assign({}, {interval:1000}, this.options);
 
         if (isString(this.options.color)) {
             try {
@@ -40,7 +41,7 @@ module.exports = class extends Animation {
         var pixels = new Pixels(this.strip.width, this.strip.height);
 
 
-        if (this.time == undefined || now - this.time > 1000) {
+        if (this.time == undefined || now - this.time > this.options.interval) {
             this.time = now;
 
             pixels.fill(this.color);
