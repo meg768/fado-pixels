b2744392eb42fc383b9334085ef84568b84769f6
meg768 <meg768@users.noreply.github.com>
2017-10-26 19:11:18 +0200

    -

--- test.js
+++ test.js
@@ -11,42 +11,6 @@ function debug() {
     console.log.apply(this, arguments);
 }
 
-/*
-class FileMonitor extends Events {
-
-	constructor(fileName) {
-		super();
-
-
-		var path = Path.dirname(fileName);
-
-		console.log('Monitoring', path);
-
-		Watch.createMonitor(path, (monitor) => {
-
-			monitor.on('created', (file, stat) => {
-				this.emit('created', file, stat);
-				monitor.stop();
-			});
-
-			monitor.on('changed', (file, stat) => {
-				this.emit('changed', file, stat);
-				monitor.stop();
-			});
-
-			monitor.on('removed', (file, stat) => {
-				this.emit('removed', file, stat);
-				monitor.stop();
-			});
-
-
-		});
-
-	}
-
-
-};
-*/
 
 var App = function() {
 
