01b9ed4a85c6935e7a3a045826c0e83163fe6e24
meg768 <meg768@users.noreply.github.com>
2017-10-26 22:01:34 +0200

    -

--- src/scripts/neopixel-strip.js
+++ src/scripts/neopixel-strip.js
@@ -27,9 +27,9 @@ module.exports = function NeopixelStrip(options) {
 	_this.width  = _width;
 	_this.height = _height;
 
-	process.on('SIGINT', function () {
-		_strip.render(new Uint32Array(_length));
-	});
+	//process.on('SIGINT', function () {
+		//_strip.render(new Uint32Array(_length));
+	//});
 
 	_this.render = function(pixels, options) {
 
