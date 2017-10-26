bedc26767528549466342685b84ec62a3975e232
meg768 <meg768@users.noreply.github.com>
2017-10-27 00:07:27 +0200

    -

--- src/commands/server.js
+++ src/commands/server.js
@@ -150,7 +150,8 @@ var Module = new function() {
 
 			setup.on('connecting', () => {
 				debug('Connecting to Wi-Fi...');
-                enqueue(new BlinkAnimation(strip, {priority:'!', frequency: 4000, color:'orange', duration:-1}));
+                //enqueue(new BlinkAnimation(strip, {priority:'!', frequency: 4000, color:'orange', duration:-1}));
+                enqueue(new PulseAnimation(strip, {priority:'!', frequency: 100, color:'orange', duration:-1}));
 			});
 
             setup.on('discoverable', () => {
