21d211da33786ddd2975cba58f004e3fb1feece6
meg768 <meg768@users.noreply.github.com>
2017-10-27 19:03:49 +0200

    -

--- src/scripts/random-animation.js
+++ src/scripts/random-animation.js
@@ -6,14 +6,20 @@ var Strip     = require('./neopixel-strip.js');
 var Pixels    = require('./pixels.js');
 var Color     = require('color');
 
+function debug() {
+    console.log.apply(this, arguments);
+}
+
 module.exports = class extends Animation {
 
     constructor(strip, options) {
         super(strip, options);
 
         this.name       = 'Random';
-        this.lastRender = 0;
         this.hue        = 0;
+
+        this.time       = new Date();
+        this.tick       = 0;
 
         console.log('New random animation', this.options);
 
@@ -21,9 +27,18 @@ module.exports = class extends Animation {
 
     render() {
 
+        var now = new Date();
+
+        this.tick++;
+
         this.hue = (this.hue + 1) % 360;
         this.pixels.fill(Color.hsl(this.hue, 100, 50).rgbNumber());
         this.strip.render(this.pixels.getPixels());
+
+        if ((this.ticks % 1000) == 0) {
+            var fps = this.ticks / (this.time.valueOf() - now.valueOf()) / 1000;
+            debug('Frames per second:', tps);
+        }
     }
 
 
