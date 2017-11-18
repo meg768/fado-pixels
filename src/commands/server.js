2adcb3dc8db327be2bc29b60dc07e350d517e283
meg768 <meg768@users.noreply.github.com>
2017-11-18 16:30:08 +0100

    -

--- src/commands/server.js
+++ src/commands/server.js
@@ -61,8 +61,8 @@ var Module = new function() {
             var duration         = 60000;
 			var onoff            = true;
 
-			//var button           = new Button({autoEnable:true, pin:19});
-			/*var button           = new Button(19);
+			var button           = new Button({autoEnable:true, pin:19});
+			//var button           = new Button(19);
 
 			button.on('click', () => {
 				debug('Click!!');
@@ -78,7 +78,7 @@ var Module = new function() {
 				onoff = !onoff;
 			});
 
-			*/
+
 			function runNextAnimation() {
 
 				animationIndex = (animationIndex + 1) % animations.length;
