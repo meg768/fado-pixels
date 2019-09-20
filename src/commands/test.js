dde2658947b61f94c8bd852aae920d623e22eb28
meg768 <meg768@users.noreply.github.com>
2019-09-20 16:23:39 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -8,9 +8,12 @@ var isFunction       = require('yow/is').isFunction;
 var Strip            = require('rpi-neopixels').Strip;
 var AnimationQueue   = require('rpi-neopixels').AnimationQueue;
 var Gpio       = require('pigpio').Gpio;
+var SoundSensor = require('../scripts/sound-sensor.js');
 
 function debug() {
 }
+
+
 
 var Module = new function() {
 
@@ -31,46 +34,13 @@ var Module = new function() {
 
 	function run(argv) {
 
-		var pin      = 19;
-		var timeout = null;
-		//var gpio     = new Gpio(pin, {mode: Gpio.INPUT, pullUpDown: Gpio.PUD_UP, edge: Gpio.RISING_EDGE});
-		var gpio     = new Gpio(pin, {mode: Gpio.INPUT, alert:true});
 
+		var sensor = new SoundSensor({pin:19});
 		console.log('Test started.')
 
-//		gpio.glitchFilter(10000);
-		gpio.on('interrupt', (state, time) => {
-
-
-			console.log('interrupt', state, time);
-		});		
-
-		var startTick = null;
-		gpio.on('alert', (level, tick) => {
-
-			if (level > 0) {
-				//console.log('alert', level, tick);
-				if (timeout != null) {
-					clearTimeout(timeout);
-					timeout = null;
-				}
-				else {
-					//console.log('First tick', tick);
-					startTick = tick;
-				}
-				timeout = setTimeout(() => {
-					//console.log('Last tick', tick);
-					var endTick = tick;
-					var diff = (endTick >> 0) - (startTick >> 0);
-					console.log('dB-ish', diff);
-					clearTimeout(timeout);
-					timeout = null;
-
-				}, 100);
-				
-			}
+		sensor.on('sound', (duration) => {
+			console.log('Sound detected', duration);
 		});
-
 
 	}
 
---
+++ src/scripts/sound-sensor.js
@@ -0,0 +1,55 @@
+var Timer      = require('yow/timer');
+var Events     = require('events');
+var Gpio       = require('pigpio').Gpio;
+
+module.exports = class SoundSensor extends Events {
+
+	constructor(options) {
+
+		super();
+
+		options = Object.assign({}, options);
+
+		if (options.pin == undefined)
+			throw new Error('Must supply a pin number.');
+
+		//var gpio = new Gpio(options.pin, {mode: Gpio.INPUT, pullUpDown: Gpio.PUD_DOWN, edge: Gpio.EITHER_EDGE});
+		var gpio = new Gpio(options.pin, {mode: Gpio.INPUT, alert: true});
+
+		var timeout = null;
+		var timestamp = null;
+
+		gpio.on('alert', (level, tick) => {
+			if (level > 0) {
+
+				//console.log('alert', level, tick);
+				if (timeout != null) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+
+				if (timestamp == null) {
+					console.log('First tick', tick);
+					timestamp = tick;
+				}
+
+				timeout = setTimeout(() => {
+					//console.log('Last tick', tick);
+					var duration = (tick >> 0) - (timestamp >> 0);
+					console.log('dB-ish', duration);
+
+					this.emit('sound', duration);
+
+					clearTimeout(timeout);
+					timeout = null;
+					timestamp = null;
+
+				}, 100);
+				
+			}
+
+		});
+	}
+
+
+};
