ddeed08a9795d16a0aabf5221bc6fbc4ddb9b7af
meg768 <meg768@users.noreply.github.com>
2019-09-20 20:12:36 +0200

    -

--- src/commands/test.js
+++ src/commands/test.js
@@ -34,7 +34,7 @@ var Module = new function() {
 
 	function run(argv) {
 		var SoundSensor = require('../scripts/pigpio-vma309.js');
-		var sensor = new SoundSensor({pin:19, xdebug:console.log});
+		var sensor = new SoundSensor({pin:19, timeout:50, xdebug:console.log});
 
 		sensor.on('alert', (duration) => {
 			console.log('Sound detected', duration);
