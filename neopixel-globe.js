db2594aedec0c5f5ebba74fa795c8ae5ceff7978
meg768 <meg768@users.noreply.github.com>
2019-09-26 20:12:24 +0200

    -

--- neopixel-globe.js
+++ neopixel-globe.js
@@ -21,6 +21,7 @@ var App = function() {
 
 			args.command(require('./src/commands/server.js'));
 			args.command(require('./src/commands/blink.js'));
+			args.command(require('./src/commands/pulse.js'));
 			args.command(require('./src/commands/test.js'));
 
 			args.wrap(null);
---
+++ src/commands/pulse.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+var Path             = require('path');
+var sprintf          = require('yow/sprintf');
+var isObject         = require('yow/is').isObject;
+var isString         = require('yow/is').isString;
+var isFunction       = require('yow/is').isFunction;
+var Neopixels        = require('../scripts/neopixels.js');
+var AnimationQueue   = require('rpi-animations').Queue;
+var PulseAnimation   = require('../scripts/pulse-animation.js');
+
+function debug() {
+}
+
+var Module = new function() {
+
+
+
+	function defineArgs(args) {
+
+		args.help('help').alias('help', 'h');
+
+
+		args.wrap(null);
+
+		args.check(function(argv) {
+			return true;
+		});
+	}
+
+
+	function run(argv) {
+
+        if (argv.debug) {
+            debug = function() {
+                console.log.apply(this, arguments);
+            }
+        }
+
+		var pixels    = new Neopixels();
+		var queue     = new AnimationQueue({debug:argv.debug});
+		var duration  = 60000;
+
+		var animation = new PulseAnimation({pixels:pixels, duration:duration, priority:'!'});
+
+		queue.enqueue(animation);
+
+	}
+
+
+	module.exports.command  = 'fade [options]';
+	module.exports.describe = 'Fade';
+	module.exports.builder  = defineArgs;
+	module.exports.handler  = run;
+
+};
