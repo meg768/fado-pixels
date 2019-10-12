8875bf440b5c39960aa4e1da332eaaa8b5885985
meg768 <meg768@users.noreply.github.com>
2019-10-12 21:01:40 +0200

    -

--- src/scripts/clock-animation.js
+++ src/scripts/clock-animation.js
@@ -8,19 +8,19 @@ module.exports = class extends ColorAnimation {
         super({name:'Clock Animation', renderFrequency: 1 * 1000, ...options});
     }
 
-    getColor() {
-        return Color.hsl([this.getHue(), 100, 50]).rgbNumber();
-    }
-
     getHue() {
         var now = new Date();
         return Math.floor(360 * (((now.getHours() % 12) * 60) + now.getMinutes()) / (12 * 60));
     }
 
+
+    getColor() {
+        return Color.hsl([this.getHue(), 100, 50]).rgbNumber();
+    }
+
+
     render() {
         this.color = this.getColor();
-
-        console.log(this.color);
         super.render();
     }
 
