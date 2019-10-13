1c4b26f85deb64f0b04be7d72f5d2315868296d5
meg768 <meg768@users.noreply.github.com>
2019-10-13 21:27:07 +0200

    -

--- src/scripts/blink-animation.js
+++ src/scripts/blink-animation.js
@@ -5,24 +5,39 @@ var Sleep = require('sleep');
 module.exports = class extends ColorAnimation {
 
     constructor(options) {
-        var {length = 500, ...options} = options;
+        var {length = 500, fade, fadeIn, fadeOut, fadeInOut, ...options} = options;
         super({name:'Blink Animation', renderFrequency:500, ...options});
 
-        this.state = 0;
-        this.length = length;
+        if (fade)
+            fadeIn = fadeOut = fade;
+    
+        if (fadeInOut)
+            fadeIn = fadeOut = fadeInOut;
+
+
+        this.length  = length;
+        this.fadeIn  = fadeIn;
+        this.fadeOut = fadeOut;
     }
 
 
 
     render() {
         this.pixels.fill(this.color);
-        this.pixels.render({transition:'fade', duration:100});
+
+        if (this.fadeIn)
+            this.pixels.render({transition:'fade', duration:this.fadeIn});
+        else
+            this.pixels.render();
 
         Sleep.msleep(this.length);
         
         this.pixels.fill(0);
-        this.pixels.render({transition:'fade', duration:100});
 
+        if (this.fadeOut)
+            this.pixels.render({transition:'fade', duration:this.fadeOut});
+        else
+            this.pixels.render();
     }
 
 
