03fc4dd9beea6c6aca2001576c30b52f26c3f017
meg768 <meg768@users.noreply.github.com>
2019-10-14 17:31:08 +0200

    -

--- src/commands/blink.js
+++ src/commands/blink.js
@@ -2,25 +2,6 @@
 
 var CLI = require('../scripts/cli.js');
 
-class CLIX {
-
-	constructor(options) {
-		var {command, describe} = options;
-
-		module.exports.command  = command;
-		module.exports.describe = describe;
-		module.exports.builder  = this.defineArgs.bind(this);
-		module.exports.handler  = this.run.bind(this);
-	}
-
-	defineArgs(args) {
-		args.help('help').alias('help', 'h');
-	}
-
-	run(argv) {
-	}
-}
-
 class BlinkCommand extends CLI {
 
 	constructor() {
--- src/scripts/cli.js
+++ src/scripts/cli.js
@@ -3,7 +3,10 @@ module.exports = class CLI {
 	constructor(options) {
 		var {module, command, describe} = options;
 
-		module.exports.command  = command;
+		if (!module || !command || !describe)
+			throw new Error('The module, command name and description must be specified.');
+
+			module.exports.command  = command;
 		module.exports.describe = describe;
 		module.exports.builder  = this.defineArgs.bind(this);
 		module.exports.handler  = this.run.bind(this);
