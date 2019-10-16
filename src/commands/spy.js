c3feb25e24fa064498c2b59bf639c7147235c0bf
meg768 <meg768@users.noreply.github.com>
2019-10-16 20:22:27 +0200

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -2,15 +2,159 @@ var Command          = require('../scripts/command.js');
 var Button           = require('pigpio-button');
 var Neopixels        = require('../scripts/neopixels.js');
 var AnimationQueue   = require('../scripts/animation-queue.js');
-var ColorAnimation   = require('../scripts/color-animation.js');
+var Animation        = require('../scripts/animation.js');
+var Yahoo            = require('yahoo-finance');
 
-class SpyAnimation extends ColorAnimation {
+class SpyAnimation extends Animation {
 
+	constructor(options) {
+		var {updateInterval = 10000, symbol = 'SPY'} = options;
 
-	getColor() {
-		
+		super({renderFrequency: 60000, ...options});
+
+		this.lastQuote = undefined;		
+		this.log = console.log;
+		this.debug = console.log;
+		this.color = 'purple';
+		this.symbol = symbol;
+		this.updateInterval = updateInterval;
+		this.symbol = symbol;
+
+		this.updateLoop();
+
 	}
+
+    computeColor(quote) {
+        var change     = Math.max(-1, Math.min(1, quote.change));
+        var hue        = change >= 0 ? 240 : 0;
+        var saturation = 100;
+        var luminance  = 0 + (Math.abs(change) * 50);
+
+        luminance = 100;
+        saturation = 50 + (Math.abs(change) * 50);
+
+        return {hue:hue, saturation:saturation, luminance:luminance}
+    }
+
+
+    fetch(symbol) {
+
+        var then = new Date();
+
+		return new Promise((resolve, reject) => {
+            try {
+                var options = {};
+
+                options.symbol = symbol;
+                options.modules = ['price', 'summaryProfile', 'summaryDetail'];
+  
+				this.debug(`Fetching quotes for ${symbol}...`);
+
+                Yahoo.quote(options).then((data) => {
+                    var now = new Date();
+                    var time = Math.floor(now.valueOf() - then.valueOf());
+
+                    var quote = {};
+                    quote.symbol = symbol;
+                    quote.name = data.price.longName ? data.price.longName : data.price.shortName;
+                    quote.sector = data.summaryProfile ? data.summaryProfile.sector : 'n/a';
+                    quote.industry = data.summaryProfile ? data.summaryProfile.industry : 'n/a';
+                    quote.exchange = data.price.exchangeName;
+                    quote.type = data.price.quoteType.toLowerCase().replace(/\b\w/g, l => l.toUpperCase());
+                    quote.change = data.price.regularMarketChangePercent * 100;
+                    quote.time = data.price.regularMarketTime;
+                    quote.price = data.price.regularMarketPrice;
+
+                    // Fix some stuff
+                    quote.name = quote.name.replace(/&amp;/g, '&');
+
+                    this.log(sprintf('Fetched quote from Yahoo for symbol %s (%s%.2f%%). Took %d ms.', quote.symbol, quote.change >= 0 ? '+' : '-', parseFloat(Math.abs(quote.change)), time));
+    
+                    resolve(quote);
+    
+                })
+                .catch((error) => {
+                    this.log(sprintf('Could not get general information about symbol %s. %s', symbol, error.message));
+                    reject(error);
+                });
+    
+            }
+            catch (error) {
+                reject(error);
+            }
+		})
+	}
+
+
+    update() {
+        return new Promise((resolve, reject) => {
+
+            try {
+
+                this.fetch(this.symbol).then((quote) => {
+
+                    var color = this.computeColor(quote);
+
+                    // Set to blue when market closed...
+                    if (false) {
+                        if (this.lastQuote && quote.time) {
+                            if (this.lastQuote.time.valueOf() == quote.time.valueOf()) {
+                                color = {red:0, green:0, blue:5};
+                            }
+                        }
+    
+                    }
+                    
+                    this.lastQuote = quote;
+
+                    return color;
+                })
+                .then((color) => {
+                    var now = new Date();
+                    var hour = now.getHours();
+
+                    if (hour >= 9 && hour <= 22)
+                        return color;
+                    else
+                        return ({hue:0, saturation:0, luminance:0});
+
+                })
+                .then((color) => {
+                    this.color = color;
+
+                })
+                .then(() => {
+                    resolve();
+                })
+                .catch((error) => {
+                    resolve();
+                })
+    
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+	updateLoop() {
+        // Get update interval from config, default 5 minutes
+        var updateInterval = (parseFloat(this.updateInterval) || 5) * 60000;
+
+        this.update().then(() => {
+            setTimeout(this.updateLoop.bind(this), updateInterval);
+        })
+        .catch((error) => {
+            this.log(error.stack);
+            setTimeout(this.updateLoop.bind(this), updateInterval);
+        });
+
+    }
+
+
 	render() {
+        this.pixels.fill(this.color);
+        this.pixels.render();
 
 	}
 
