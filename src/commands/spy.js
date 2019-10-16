f028d1e5844acd4b0d295a9c60e33fea54af39e7
meg768 <meg768@users.noreply.github.com>
2019-10-16 20:47:02 +0200

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -150,6 +150,7 @@ class SpyAnimation extends Animation {
 	render() {
 		this.log('Rendering SPY.', this.color);
         this.pixels.fill(this.color);
+        this.pixels.fill(Color.hsl(0, 50, 50));
         this.pixels.render();
 
 	}
