dbe7d5aed3d2feec547e8b0c3bdde2c607a19f8f
meg768 <meg768@users.noreply.github.com>
2019-10-17 16:23:07 +0200

    -

--- neopixel-globe.js
+++ neopixel-globe.js
@@ -42,8 +42,8 @@ var App = function() {
 
 			//args.command(require('./src/commands/server.js'));
 			args.command(require('./src/commands/blink.js'));
-			args.command(require('./src/commands/pulse.js'));
-			args.command(require('./src/commands/clock.js'));
+			//args.command(require('./src/commands/pulse.js'));
+			//args.command(require('./src/commands/clock.js'));
 //			args.command(require('./src/commands/spy.js'));
 
 			args.demandCommand(1);
