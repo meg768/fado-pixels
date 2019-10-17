96cdf8b72f432004059bba5f172ba71727e9ff5f
meg768 <meg768@users.noreply.github.com>
2019-10-17 21:39:54 +0200

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -14,12 +14,13 @@ class SpyAnimation extends Animation {
 	constructor(options) {
 		var {symbol = 'SPY', ...options} = options;
 
-		super({name:'Spy Animation', renderFrequency: 30000, ...options});
+		super({name:'Spy Animation', renderFrequency: 10000, ...options});
 
 		this.log = console.log;
 		this.debug = console.log;
         this.symbol = symbol;
         this.cache = cache;
+
 
 
 	}
