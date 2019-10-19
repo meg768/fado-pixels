009e3484079b51a863716dade3f9e651695d8353
meg768 <meg768@users.noreply.github.com>
2019-10-19 18:51:34 +0200

    -

--- src/scripts/fado.js
+++ src/scripts/fado.js
@@ -1,14 +1,28 @@
-var Neopixels        = require('../scripts/neopixels.js');
-var AnimationQueue   = require('rpi-animations').Queue;
 
 module.exports = class Fado {
 	constructor(options) {
-		this.pixels  = new Neopixels();
+		var Neopixels        = require('../scripts/neopixels.js');
+		var AnimationQueue   = require('rpi-animations').Queue;
+		
+		this.pixels  = new Neopixels({debug:options.debug});
 		this.queue   = new AnimationQueue({debug:options.debug});
 	}
 
 	runAnimation(animation) {
 		this.queue.enqueue(animation);
-	};
+	}
+
+	blink(options) {
+		var Animation = require('./blink-animation.js'); 
+		this.runAnimation(new Animation(options));
+	}
+
+	color(options) {
+		var Animation = require('./color-animation.js'); 
+		this.runAnimation(new Animation(options));
+	}
+
+
+
 }
 
