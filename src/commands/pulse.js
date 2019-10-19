71864c9497934a84c6a1c385cec6686c5e578b70
meg768 <meg768@users.noreply.github.com>
2019-10-19 17:44:17 +0200

    -

--- src/commands/pulse.js
+++ src/commands/pulse.js
@@ -5,19 +5,19 @@ var Command          = require('../scripts/command.js');
 
 class PulseCommand extends Command {
 	constructor() {
-		var config = {
+		var defaults = {
 			color      : 'red',
 			duration   : 60000,
 			interval   : 500
 		};
 
-		super({module:module, name: 'pulse', description:'Pulse light', config:config});
+		super({module:module, name: 'pulse', description:'Pulse light', defaults:defaults});
 	}
 
 	defineArgs(args) {
-		args.option('color', {describe:'Color', default:this.config.color});
-		args.option('duration', {describe:'Duration', default:this.config.duration});
-		args.option('interval', {describe:'Interval', default:this.color.interval});
+		args.option('color', {describe:'Color', default:this.defaults.color});
+		args.option('duration', {describe:'Duration', default:this.defaults.duration});
+		args.option('interval', {describe:'Interval', default:this.defaults.interval});
 	}
 
 	run(argv) {
