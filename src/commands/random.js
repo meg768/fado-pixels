03fde5066158606b384d138031428995f9aaa4a8
meg768 <meg768@users.noreply.github.com>
2019-10-19 17:25:53 +0200

    -

--- src/commands/random.js
+++ src/commands/random.js
@@ -1,37 +1,37 @@
 var Command = require('../scripts/command.js');
 
-class RandomAnimation extends Command {
+class RandomCommand extends Command {
 
 	constructor() {
 		var defaults = {
-			duration: 60000
+			color           : 'green',
+			duration        : 10000
 		};
 
-		super({module:module, name: 'random', description:'Random color spinner', defaults:defaults});
+		super({module:module, name: 'color', description:'Set color', defaults:defaults});
 
 
 	}
 
 	defineArgs(args) {
 		args.option('duration', {describe:'Duration', default:this.defaults.duration});
-
+		args.option('color', {describe:'Color', default:this.defaults.color});
 	}
 
 
 	run(argv) {
-		console.log('ARGV', argv);
-		
 		var Neopixels        = require('../scripts/neopixels.js');
 		var AnimationQueue   = require('../scripts/animation-queue.js');
+		var RandomAnimation  = require('../scripts/random-animation.js');
 		
 		var queue      = new AnimationQueue({debug:argv.debug});
-		var options    = {pixels:new Neopixels(), priority:'!', ...argv};	
+		var options    = {pixels:new Neopixels(), renderFrequency:1000, priority:'!', ...argv};	
+		var animation  = new RandomAnimation(options);
 	
-		queue.enqueue(new RandomAnimation(options));
+		queue.enqueue(animation);
 
 	}
 }
 
-
-new RandomAnimation();
+new RandomCommand();
 
