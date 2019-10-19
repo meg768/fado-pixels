5bb38ed4120d35897b0281c8403831e9defb90f5
meg768 <meg768@users.noreply.github.com>
2019-10-19 18:28:07 +0200

    -

--- src/commands/blink.js
+++ src/commands/blink.js
@@ -1,7 +1,9 @@
-var Command = require('../scripts/command.js');
+var Neopixels        = require('../scripts/neopixels.js');
+var AnimationQueue   = require('rpi-animations').Queue;
+var PulseAnimation   = require('../scripts/blink-animation.js');
+var Command          = require('../scripts/command.js');
 
-class BlinkCommand extends Command {
-
+class PulseCommand extends Command {
 	constructor() {
 		var defaults = {
 			color      : 'red',
@@ -9,31 +11,23 @@ class BlinkCommand extends Command {
 			interval   : 500
 		};
 
-		super({module:module, name: 'blink', description:'Blink', defaults:defaults});
-
-
+		super({module:module, name: 'pulse', description:'Pulse light', defaults:defaults});
 	}
 
 	defineArgs(args) {
-		args.option('iterations', {describe:'Iterations', default:this.defaults.iterations});
-		args.option('duration', {describe:'Duration', default:this.defaults.duration});
 		args.option('color', {describe:'Color', default:this.defaults.color});
-
+		args.option('duration', {describe:'Duration', default:this.defaults.duration});
+		args.option('interval', {describe:'Interval', default:this.defaults.interval});
 	}
-
 
 	run(argv) {
-		var Neopixels        = require('../scripts/neopixels.js');
-		var AnimationQueue   = require('../scripts/animation-queue.js');
-		var BlinkAnimation   = require('../scripts/blink-animation.js');
-		
-		var queue      = new AnimationQueue({debug:argv.debug});
-		var options    = {pixels:new Neopixels(), ...argv};	
-	
-		queue.enqueue(new BlinkAnimation(options));
+		var pixels    = new Neopixels();
+		var queue     = new AnimationQueue({debug:argv.debug});
+		var animation = new PulseAnimation({pixels:pixels, ...argv});
 
-	}
+		queue.enqueue(animation);
+
+	};
 }
 
-new BlinkCommand();
-
+new PulseCommand();
