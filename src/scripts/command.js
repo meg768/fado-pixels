7d2062b3b1f0f8982d2ab55c65b938b15e23bfcb
meg768 <meg768@users.noreply.github.com>
2019-10-20 17:36:12 +0200

    -

--- src/scripts/command.js
+++ src/scripts/command.js
@@ -20,7 +20,7 @@ module.exports = class Command {
 		module.exports.builder  = (yargs) => {
 			yargs.wrap(null);
 			yargs.option('help',  {describe:'Display help', default:false});
-			yargs.option('debug', {describe:'Debug mode', boolean:true, default:false});
+			yargs.option('debug', {describe:'Debug mode', boolean:true, default:true});
 	
 
 			this.defineArgs(yargs);
