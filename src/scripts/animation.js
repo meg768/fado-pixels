7f0e63cca839e450278c590d2339c4380fe6bea3
meg768 <meg768@users.noreply.github.com>
2019-10-21 19:31:50 +0200

    -

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -66,11 +66,11 @@ class Animation extends Events {
             var render = () => {
                 var now = new Date();
 
-                if (this.renderFrequency == undefined || this.renderFrequency == 0 || this.renderTime == undefined || now - this.renderTime >= this.renderFrequency) {
+//                if (this.renderFrequency == undefined || this.renderFrequency == 0 || this.renderTime == undefined || now - this.renderTime >= this.renderFrequency) {
                     this.debug(`Rendering ${this.name}...`);
                     this.render();
                     this.renderTime = now;
-                }
+  //              }
 
             };
 
