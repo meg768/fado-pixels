c6469c9ec5f252881f911bb8c1f8f72899d575f7
meg768 <meg768@users.noreply.github.com>
2019-10-21 20:03:36 +0200

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -72,7 +72,7 @@ class SpyCommand extends Command {
 			}
 
 			var color = computeColorFromQuote(quote);
-			fado.color({color:color, fade:500, renderFrequency:10000, duration:-1, priority:'highXXX'});
+			fado.color({color:color, fade:500, renderFrequency:10000, duration:-1, priority:'!'});
 		});
 
 		quotes.startMonitoring();
