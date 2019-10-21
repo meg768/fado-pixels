992afedc81a6fb87d800a38fa37f617d857cbab9
meg768 <meg768@users.noreply.github.com>
2019-10-21 19:46:42 +0200

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -76,7 +76,7 @@ class SpyCommand extends Command {
 		});
 		quotes.startMonitoring();
 */
-		fado.color({color:offlineColor, fade:500, duration:-1});
+		fado.color({color:offlineColor, fade:500, renderFrequency:0, duration:-1});
 
 
 	}
