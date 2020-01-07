6e9346417de3fcb873fa864b6e81751289bd6b21
meg768 <meg768@users.noreply.github.com>
2020-01-07 23:28:18 +0100

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -56,7 +56,7 @@ this.express.use(function (req, res, next) {
 
 	console.log('Adding stuff!!');
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
