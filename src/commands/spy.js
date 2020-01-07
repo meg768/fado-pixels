3005466b322eb869c4b1aa4480798f867195f260
meg768 <meg768@users.noreply.github.com>
2020-01-07 21:14:53 +0100

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -53,8 +53,13 @@ var allowCrossDomain = function(req, res, next) {
 		this.express = Express();
 		this.express.use(BodyParser.json());
 		this.express.use(Cors());
-		this.express.use(allowCrossDomain());
+		//this.express.use(allowCrossDomain());
 
+		this.express.all('/', function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "X-Requested-With");
+			next();
+		});
 
 		this.express.post('/blink', (request, response) => {
 			this.fado.blink(request.body);
