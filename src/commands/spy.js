77eea5f95a05314d16fe7a9a69d23878f3aceb69
meg768 <meg768@users.noreply.github.com>
2020-01-07 23:07:53 +0100

    -

--- src/commands/spy.js
+++ src/commands/spy.js
@@ -49,7 +49,28 @@ class Spy {
 		this.express = Express();
 		this.express.use(BodyParser.json());
 		this.express.use(Cors());
-		this.express.use(allowCrossDomain);
+//		this.express.use(allowCrossDomain);
+
+		// Add headers
+this.express.use(function (req, res, next) {
+
+	
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 		this.express.all('*', function(req, res, next) {
 			console.log('ALLOWING ALLLLLLLLLLL');
