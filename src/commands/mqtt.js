911c05b1f7f1753b6edbf57a627cefec76b7b8e6
meg768 <meg768@users.noreply.github.com>
2024-01-04 19:28:13 +0100

    -

---
+++ src/commands/mqtt.js
@@ -0,0 +1,26 @@
+var Command = require('../scripts/command.js');
+
+
+class MqttCommand extends Command {
+
+	constructor() {
+		var defaults = {
+			port:3000
+		};
+		super({ module: module, name: 'server', description: 'Fado server', defaults: defaults});
+	}
+
+	defineArgs(yargs) {
+		yargs.option('port', { describe: 'Port to listen to', default: this.defaults.port});
+	}
+
+	run(argv) {
+        let args = {};
+		var Fado = require('../scripts/fado.js')
+		var fado = new Fado();
+        fado.blink();
+	}
+
+}
+
+new MqttCommand();
