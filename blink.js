458aaf5ccb58c346cdb6d9327d77eb472ed3c8a3
meg768 <magnus@egelberg.se>
2024-07-13 19:50:47 +0200

    -

--- blink.js
+++ blink.js
@@ -37,9 +37,9 @@ class App {
 			var Fado = require('./src/scripts/fado.js');
 			var fado = new Fado({});
 
-			var config = require('./config.json');
-			console.log(config);
-			
+			var {neopixels}  = require('./config.json');
+			console.log(neopixels);
+
 			var options = {
 				color      : 'red',
 				duration   : 60000,
