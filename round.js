dbfab56a78759d7510dd6617865df5a9c3af035a
meg768 <magnus@egelberg.se>
2024-07-15 23:04:34 +0200

    -

--- round.js
+++ round.js
@@ -24,7 +24,7 @@ class RoundAnimation extends Animation {
         this.pixels.fill(Color("black").rgbNumber());
 
 		for (let i = 0; i < 24; i++) {
-			let color = Color(Random(["black", "black", "black", "red", "orange", "yellow"])).rgbNumber();
+			let color = Color(Random(["black", "black", "black", "black", "black", "red", "orange", "yellow"])).rgbNumber();
 			this.pixels.setPixel(i, 0, color);
 		}
         //this.pixels.render();
