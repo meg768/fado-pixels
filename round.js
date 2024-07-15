fb3d223d349f7558caa75023dba62f73cfd2cebd
meg768 <magnus@egelberg.se>
2024-07-15 23:09:19 +0200

    -

--- round.js
+++ round.js
@@ -16,7 +16,7 @@ class RoundAnimation extends Animation {
 
 	async stop() {
 		this.pixels.clear();
-		this.pixels.render();
+        this.pixels.render({transition:'fade', duration:500});
 		await super.stop();
 	}
 
