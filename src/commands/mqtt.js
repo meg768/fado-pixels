9eb8baa162c81a2be597873682e0b3a1671ea9fb
meg768 <magnus@egelberg.se>
2024-12-13 16:10:24 +0100

    -

--- src/commands/mqtt.js
+++ src/commands/mqtt.js
@@ -1,5 +1,6 @@
 var Command = require('../scripts/command.js');
 var Fado = require('../scripts/fado.js')
+var MQTT = require('mqtt');
 
 
 class MqttCommand extends Command {
@@ -12,13 +13,60 @@ class MqttCommand extends Command {
 	}
 
 	defineArgs(yargs) {
-		yargs.option('port', { describe: 'Port to listen to', default: this.defaults.port});
+
+		yargs.option('host',     {describe:'Specifies MQTT host', default:process.env.MQTT_HOST});
+		yargs.option('password', {describe:'Password for MQTT broker', default:process.env.MQTT_PASSWORD});
+		yargs.option('username', {describe:'User name for MQTT broker', default:process.env.MQTT_USERNAME});
+		yargs.option('port',     {describe:'Port for MQTT', default:process.env.MQTT_PORT});
+		yargs.option('topic',    {describe:'Topic for MQTT', default:process.env.MQTT_TOPIC});
+		
 	}
 
 	run(argv) {
         let args = {};
         let fado = new Fado({log:this.log, debug:this.debug});
-        fado.color();
+
+		this.debug(`Connecting to host '${this.argv.host}'...`);
+		var mqtt = MQTT.connect(this.argv.host, {username:this.argv.username, password:this.argv.password, port:this.argv.port});
+
+		mqtt.on('connect', () => {
+			this.log(`Connected to ${this.argv.host}:${this.argv.port}...`);
+//			this.displayText(`Listening to MQTT topic ${this.argv.topic}... 🤪`);
+		})
+
+		mqtt.subscribe(this.argv.topic, (error) => {
+
+        });
+
+
+		mqtt.on('message', (topic, message) => {
+			try {
+                message = message.toString();
+
+                this.log(`MQTT message: '${message}'`);
+
+                if (message == '')
+                    return
+
+                try {
+                    let payload = JSON.parse(message)
+                    let {animation, ...options} = payload;
+
+                    this.runAnimation(animation, options);
+                }
+                catch(error) {
+                    this.displayText(error);
+
+                }
+
+
+            }
+			catch(error) {
+				this.log(error);
+			}
+		});
+
+
 	}
 
 }
