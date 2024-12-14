f444eebec2716c09188c51cddcc57b2c9cbdfe5c
meg768 <magnus@egelberg.se>
2024-12-14 18:21:55 +0100

    -

--- src/scripts/fado.js
+++ src/scripts/fado.js
@@ -18,6 +18,7 @@ module.exports = class Fado {
 		this.defaultAnimation.color = 'green';
 		this.defaultAnimation.fade = 500;
 		this.defaultAnimation.duration = -1;
+		this.defaultAnimation.priority = '!';
 
 		this.queue.on('idle', () => {
 			this.runDefaultAnimation();
